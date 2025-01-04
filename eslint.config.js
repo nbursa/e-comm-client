9b86fd9992f6b42513c88c275473ef9f33b5378d
Nenad <nbursa@gmail.com>
2025-01-04 20:27:44 +0100

    feat(app):  Eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import js from '@eslint/js';
 import globals from 'globals';
 import pluginVue from 'eslint-plugin-vue';
@@ -6,94 +9,43 @@ import vueTsEslintConfig from '@vue/eslint-config-typescript';
 import prettierSkipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 import prettierConfig from '@vue/eslint-config-prettier';
 
-export default [
-  {
-    /**
-     * Ignore the following files.
-     * Please note that pluginQuasar.configs.recommended() already ignores
-     * the "node_modules" folder for you (and all other Quasar project
-     * relevant folders and files).
-     *
-     * ESLint requires "ignores" key to be the only one in this object
-     */
-    // ignores: []
-  },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+export default [
   ...pluginQuasar.configs.recommended(),
   js.configs.recommended,
-
-  /**
-   * https://eslint.vuejs.org
-   *
-   * pluginVue.configs.base
-   *   -> Settings and rules to enable correct ESLint parsing.
-   * pluginVue.configs[ 'flat/essential']
-   *   -> base, plus rules to prevent errors or unintended behavior.
-   * pluginVue.configs["flat/strongly-recommended"]
-   *   -> Above, plus rules to considerably improve code readability and/or dev experience.
-   * pluginVue.configs["flat/recommended"]
-   *   -> Above, plus rules to enforce subjective community defaults to ensure consistency.
-   */
   ...pluginVue.configs['flat/recommended'],
-
-  // https://github.com/vuejs/eslint-config-typescript
   ...vueTsEslintConfig({
-    // Optional: extend additional configurations from typescript-eslint'.
-    // Supports all the configurations in
-    // https://typescript-eslint.io/users/configs#recommended-configurations
-    extends: [
-      // By default, only the recommended rules are enabled.
-      'recommended',
-      // You can also manually enable the stylistic rules.
-      // "stylistic",
-
-      // Other utility configurations, such as 'eslintRecommended', (note that it's in camelCase)
-      // are also extendable here. But we don't recommend using them directly.
-    ],
+    extends: ['recommended'],
   }),
 
   {
+    files: ['*.vue'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
-
       globals: {
         ...globals.browser,
-        ...globals.node, // SSR, Electron, config files
-        process: 'readonly', // process.env.*
-        ga: 'readonly', // Google Analytics
+        ...globals.node,
+        process: 'readonly',
+        ga: 'readonly',
         cordova: 'readonly',
         Capacitor: 'readonly',
-        chrome: 'readonly', // BEX related
-        browser: 'readonly', // BEX related
+        chrome: 'readonly',
+        browser: 'readonly',
+      },
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: path.resolve(__dirname, './tsconfig.json'),
+        tsconfigRootDir: __dirname,
+        extraFileExtensions: ['.vue'],
       },
     },
-
-    // add your custom rules here
-    // rules: {
-    //   semi: ['error', 'always'],
-    //   'prettier/prettier': ['error', { semi: true }],
-    //   'prefer-promise-reject-errors': 'off',
-    //   '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
-    //   '@typescript-eslint/no-unused-vars': 'error',
-    //   '@typescript-eslint/no-explicit-any': 'warn',
-    //   '@typescript-eslint/explicit-function-return-type': [
-    //     'error',
-    //     {
-    //       allowExpressions: true, // This allows for arrow functions without explicit return types
-    //       allowTypedFunctionExpressions: true, // Useful for typed callbacks
-    //       allowHigherOrderFunctions: true, // Useful for functional programming
-    //     },
-    //   ],
-    //   'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    //   'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // },
-
     rules: {
       'prefer-promise-reject-errors': 'off',
       '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
-
-      // allow debugger during development only
       'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     },
   },
