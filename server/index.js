30971184ba3e245f5cd1cf16765ecf5160756f54
Nenad <nbursa@gmail.com>
2025-01-07 12:38:12 +0100

    feat(app):  Email confirmation

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import process from 'process';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
 app.use(
