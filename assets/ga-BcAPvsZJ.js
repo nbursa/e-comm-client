d63715f397c1daf1688f6c07219c3300aee5592a
Nenad <nbursa@gmail.com>
2025-01-08 19:58:55 +0100

    Updates

---
+++ assets/ga-BcAPvsZJ.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-2jVxb2FH.js";const g=e(({router:o})=>{const t="G-KN141HH496";o.afterEach(a=>{try{window.gtag&&window.gtag("config",t,{page_path:a.path,page_location:window.location.href,page_title:document.title})}catch(r){console.error("GA tracking error:",r)}})});export{g as default};
