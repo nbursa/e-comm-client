058c7ce46daaa4fb9ae0959f4ff3c88e261db875
Nenad <nbursa@gmail.com>
2025-01-09 01:50:38 +0100

    Updates

--- assets/ga-DbP4AGa2.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./index-tE__rt3j.js";const i=e(({router:t})=>{const o="G-KN141HH496";t.afterEach(r=>{try{window.gtag&&window.gtag("config",o,{page_path:r.path,page_location:window.location.href,page_title:document.title})}catch(a){console.error("GA tracking error:",a)}}),console.error("GA tracking id:",o)});export{i as default};
