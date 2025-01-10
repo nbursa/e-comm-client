f1511725120f4e370e6d3d11f6c53ae96525acdd
Nenad <nbursa@gmail.com>
2025-01-10 21:21:49 +0100

    Updates

--- assets/init-DkLZBo0L.js
+++
@@ -1,1 +0,0 @@
-import{b as s,u as r}from"./index-Dln1CX-_.js";import{u as a}from"./rates-BrW5sabz.js";const i=a(),p=s(({app:o})=>{const t=r(),e=o.config.globalProperties.$q;i.loadExchangeRates(),t.initApp(e)});export{p as default};
