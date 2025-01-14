514ddbf31fa1af11ad9db408c0d42bb62142fa00
Nenad <nbursa@gmail.com>
2025-01-14 09:38:00 +0100

    Updates

--- assets/init-CF17pbsV.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-VIJ1gGOP.js";import{u as r}from"./rates-DQl6jw6J.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
