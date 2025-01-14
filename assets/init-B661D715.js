34a923bac31046ce4bdee742489a550a32e93974
Nenad <nbursa@gmail.com>
2025-01-15 00:22:49 +0100

    Updates

---
+++ assets/init-B661D715.js
@@ -0,0 +1,1 @@
+import{b as e,u as s}from"./index-B1bzm8pQ.js";import{u as r}from"./rates-BNzjwj2_.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
--- assets/init-BMHWiuW8.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-QkU6QXFf.js";import{u as r}from"./rates-DDkWy9ZI.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
