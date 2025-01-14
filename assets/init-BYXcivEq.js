d2128123b8f801e8a4c9e33df98a0797668e1403
Nenad <nbursa@gmail.com>
2025-01-14 13:27:18 +0100

    Updates

--- assets/init-BYXcivEq.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-QjTLx6b9.js";import{u as r}from"./rates-D5bRcDFj.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
