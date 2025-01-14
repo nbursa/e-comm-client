4dd43f52be8409cefcd74b0dacfe4e9359ce0c28
Nenad <nbursa@gmail.com>
2025-01-14 18:17:14 +0100

    Updates

--- assets/init-uYAjR6N6.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-Duu7A_n4.js";import{u as r}from"./rates-Dk_-CHYA.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
