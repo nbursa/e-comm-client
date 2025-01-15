36a66025e1b2bd3f9385fdc6c65196c1d9cb1e44
Nenad <nbursa@gmail.com>
2025-01-15 15:02:58 +0100

    Updates

--- assets/init-0_SVXmyS.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-YZU5K0Ui.js";import{u as r}from"./rates-CzJ6KyjL.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
