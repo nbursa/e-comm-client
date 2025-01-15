95e09b7b617240e44ce6490754d8348cdb630300
Nenad <nbursa@gmail.com>
2025-01-15 23:40:34 +0100

    Updates

--- assets/init-SjCf_8Di.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-Bo_3MkcN.js";import{u as r}from"./rates-C2TGJQZf.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
