f4062988ed39546392527129583201d8afbe5b69
Nenad <nbursa@gmail.com>
2025-01-15 11:11:38 +0100

    Updates

---
+++ assets/init-BL-kvkBx.js
@@ -0,0 +1,1 @@
+import{b as e,u as s}from"./index-D3B1opgj.js";import{u as r}from"./rates-BShLlSQf.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
