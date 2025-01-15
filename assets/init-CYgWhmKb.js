e0f79c57cc11747d44bff249af67d1edd8dff939
Nenad <nbursa@gmail.com>
2025-01-15 15:01:17 +0100

    Updates

--- assets/init-CYgWhmKb.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-DoD9PHaQ.js";import{u as r}from"./rates-DMNApaZd.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
