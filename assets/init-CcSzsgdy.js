c9e089a965a52f5f7435427342e84991f6e4eab2
Nenad <nbursa@gmail.com>
2025-01-15 19:40:58 +0100

    Updates

---
+++ assets/init-CcSzsgdy.js
@@ -0,0 +1,1 @@
+import{b as e,u as s}from"./index-BzG7df4L.js";import{u as r}from"./rates-C3BjGKLl.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
