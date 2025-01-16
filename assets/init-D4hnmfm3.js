07d418844f9311f40a1c639c421fcd1aa765ab7f
Nenad <nbursa@gmail.com>
2025-01-16 13:57:59 +0100

    Updates

--- assets/init-D4hnmfm3.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-td9DqLSI.js";import{u as r}from"./rates-8fxteb9s.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
