84895e521e91af165cf7bfc8dc85b99e4dbdcbd8
Nenad <nbursa@gmail.com>
2025-01-16 13:36:03 +0100

    Updates

--- assets/init-DUh_3MkL.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-DYKivRXB.js";import{u as r}from"./rates-D9XargGH.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
