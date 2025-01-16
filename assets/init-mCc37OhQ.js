01ec4a091380009447e37f745ea2a3abf474520c
Nenad <nbursa@gmail.com>
2025-01-16 14:19:03 +0100

    Updates

--- assets/init-mCc37OhQ.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-leGE0g0m.js";import{u as r}from"./rates-CgqfOzKk.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
