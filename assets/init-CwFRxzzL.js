69860593c1f82e21c8ba391610f824164d991e43
Nenad <nbursa@gmail.com>
2025-01-17 01:04:06 +0100

    Updates

--- assets/init-CwFRxzzL.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as s}from"./index-cQjNMZeo.js";import{u as r}from"./rates-CNNU1jiw.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
---
+++ assets/lightbox-oypGeShn.js
@@ -0,0 +1,1 @@
+import{b as t,N as s}from"./index-Chx6AD0y.js";const b=t(({app:o})=>{o.use(s)});export{b as default};
