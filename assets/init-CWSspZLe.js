0da251dc6c81aec6d2f7a49bcc9ebafa60a79eec
Nenad <nbursa@gmail.com>
2025-01-17 01:59:36 +0100

    Updates

---
+++ assets/init-CWSspZLe.js
@@ -0,0 +1,1 @@
+import{b as e,u as s}from"./index-DU6EdHr2.js";import{u as r}from"./rates-CIvl6D6l.js";const u=e(()=>{const t=r(),o=s();t.loadExchangeRates(),o.loadSettings()});export{u as default};
--- assets/theme-tXHRZLML.js
+++
@@ -1,1 +0,0 @@
-import{b as e,i as t,s as o}from"./index-CPInhod_.js";const n=e(({app:a})=>{const s=a.config.globalProperties.$q;o(s),t()});export{n as default,t as initThemeManager};
