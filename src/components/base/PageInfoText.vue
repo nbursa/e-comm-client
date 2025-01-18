37a82b00e581f82ac76a2b16203c868189c47f9a
Nenad <nbursa@gmail.com>
2025-01-18 09:21:02 +0100

    refactor(styles): Page info

--- src/components/base/PageInfoText.vue
+++ src/components/base/PageInfoText.vue
@@ -1,18 +1,30 @@
 <template>
-  <p class="tw-text-base tw-font-serif tw-leading-relaxed q-mb-md sm:tw-text-lg" :style="{ color }">
+  <p
+    class="tw-text-base tw-font-serif tw-leading-relaxed q-mb-md sm:tw-text-lg"
+    :style="{ color: `${textColor}` }"
+  >
     {{ description }}
   </p>
 </template>
 
 <script lang="ts" setup>
-defineProps({
+import { useQuasar } from 'quasar';
+import { computed } from 'vue';
+
+const props = defineProps({
   description: {
     type: String,
     required: true,
   },
   color: {
     type: String,
-    default: 'black',
+    default: '',
   },
+});
+
+const $q = useQuasar();
+
+const textColor = computed(() => {
+  return props.color || ($q.dark.isActive ? 'white' : 'black');
 });
 </script>
